c34b99efce1604c0181a420c0904d462ffb87808
drewdunn1 <72716414+drewdunn1@users.noreply.github.com>
2020-11-03 10:09:41 -0500

    Update callingcard.cs

--- callingcard.cs
+++ callingcard.cs
@@ -2,18 +2,17 @@ body{ font-family: cursive;
     background-color: skyblue;
      color: white;
 }
+.eName{
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+}
 img{
-    display: block;
-    margin: 50px;
-    margin-right: 190px;
-    margin-left: 210px;
     height: 200px;
     width: 200px;
     align: center;
 }
 .general{
-    font-size: 30px;
-    font-weight: bold;
     text-align: center;
 }
 .bio{
